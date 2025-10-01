6156ad2cefdc66dc0d836acbf3c0c0185b716e5c
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-10-01 23:32:12 +0530

    Initial Commit

---
+++ Lecture Code/04 Operators If-else Number System/src/LearningIf.java
@@ -0,0 +1,12 @@
+public class LearningIf{
+    public static void main(String[] args) {
+        boolean isMale = true;
+        String name = "Bob";
+        if(isMale){
+            System.out.print("Mr. " + name);
+        }
+        else{
+            System.out.print("Ms. " + name);
+        }
+    }
+}
